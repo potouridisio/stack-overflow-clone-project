fc96117b2aa850967c34a41c9a38772c3a791a1e
Ioannis Potouridis <potouridisio@gmail.com>
2025-06-18 06:06:56 +0300

    Add route headings, add Ask Question link

--- src/routes/Questions.jsx
+++ src/routes/Questions.jsx
@@ -21,47 +21,58 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="divide-y divide-gray-300 rounded border border-gray-300">
-      {questions.map((question) => (
-        <div className="flex gap-4 p-4" key={question.id}>
-          <div className="w-32 flex-none">
-            <ul className="flex flex-col items-end gap-2 py-1 text-sm">
-              <li>{question.voteCount} votes</li>
-              <li>
-                {question.answerCount > 0 ? (
-                  <div className="inline-flex h-6 items-center rounded border border-green-700 px-2 text-xs font-medium text-green-700">
-                    {question.answerCount} answer
-                    {question.answerCount === 1 ? "" : "s"}
-                  </div>
-                ) : (
-                  <div className="text-gray-500">0 answers</div>
-                )}
-              </li>
-            </ul>
-          </div>
-          <div>
-            <a
-              className="mb-1 block text-xl text-blue-500 hover:text-blue-700"
-              href="#"
-            >
-              {question.title}
-            </a>
-            <p className="mb-2 line-clamp-2 text-gray-700">{question.body}</p>
-            <ul className="flex items-center gap-2">
-              {question.tagIds.map((tagId) => (
-                <li className="inline-flex" key={tagId}>
-                  <a
-                    className="inline-flex h-6 items-center rounded bg-gray-100 px-2 text-xs font-medium text-gray-700 hover:bg-gray-300 hover:text-gray-900"
-                    href="#"
-                  >
-                    {tags[tagId].name}
-                  </a>
+    <>
+      <div className="mb-4 flex items-center">
+        <h1 className="grow text-3xl">Newest Questions</h1>
+        <a
+          className="rounded bg-blue-500 px-4 py-2 text-sm font-medium text-white hover:bg-blue-700"
+          href="#"
+        >
+          Ask Question
+        </a>
+      </div>
+      <div className="divide-y divide-gray-300 rounded border border-gray-300">
+        {questions.map((question) => (
+          <div className="flex gap-4 p-4" key={question.id}>
+            <div className="w-32 flex-none">
+              <ul className="flex flex-col items-end gap-2 py-1 text-sm">
+                <li>{question.voteCount} votes</li>
+                <li>
+                  {question.answerCount > 0 ? (
+                    <div className="inline-flex h-6 items-center rounded border border-green-700 px-2 text-xs font-medium text-green-700">
+                      {question.answerCount} answer
+                      {question.answerCount === 1 ? "" : "s"}
+                    </div>
+                  ) : (
+                    <div className="text-gray-500">0 answers</div>
+                  )}
                 </li>
-              ))}
-            </ul>
+              </ul>
+            </div>
+            <div>
+              <a
+                className="mb-1 block text-xl text-blue-500 hover:text-blue-700"
+                href="#"
+              >
+                {question.title}
+              </a>
+              <p className="mb-2 line-clamp-2 text-gray-700">{question.body}</p>
+              <ul className="flex items-center gap-2">
+                {question.tagIds.map((tagId) => (
+                  <li className="inline-flex" key={tagId}>
+                    <a
+                      className="inline-flex h-6 items-center rounded bg-gray-100 px-2 text-xs font-medium text-gray-700 hover:bg-gray-300 hover:text-gray-900"
+                      href="#"
+                    >
+                      {tags[tagId].name}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </>
   );
 }
--- src/routes/Tags.jsx
+++ src/routes/Tags.jsx
@@ -16,25 +16,33 @@ export default function Tags() {
   }, []);
 
   return (
-    <div className="grid grid-cols-4 gap-4">
-      {tags.map((tag) => (
-        <div
-          className="flex flex-col items-start space-y-2 rounded border border-gray-300 p-4 text-sm text-gray-900"
-          key={tag.id}
-        >
-          <a
-            className="inline-flex h-6 items-center rounded bg-gray-100 px-2 text-xs font-medium text-gray-700 hover:bg-gray-300 hover:text-gray-900"
-            href="#"
+    <>
+      <h1 className="mb-4 grow text-3xl">Tags</h1>
+      <p className="mb-4 max-w-2xl">
+        A tag is a keyword or label that categorizes your question with other,
+        similar questions. Using the right tags makes it easier for others to
+        find and answer your question.
+      </p>
+      <div className="grid grid-cols-4 gap-4">
+        {tags.map((tag) => (
+          <div
+            className="flex flex-col items-start space-y-2 rounded border border-gray-300 p-4 text-sm text-gray-900"
+            key={tag.id}
           >
-            {tag.name}
-          </a>
-          <p className="line-clamp-4 grow">{tag.description}</p>
-          <span className="text-xs text-gray-700">
-            {tag.occurrenceCount} question
-            {tag.occurrenceCount === 1 ? "" : "s"}
-          </span>
-        </div>
-      ))}
-    </div>
+            <a
+              className="inline-flex h-6 items-center rounded bg-gray-100 px-2 text-xs font-medium text-gray-700 hover:bg-gray-300 hover:text-gray-900"
+              href="#"
+            >
+              {tag.name}
+            </a>
+            <p className="line-clamp-4 grow">{tag.description}</p>
+            <span className="text-xs text-gray-700">
+              {tag.occurrenceCount} question
+              {tag.occurrenceCount === 1 ? "" : "s"}
+            </span>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
