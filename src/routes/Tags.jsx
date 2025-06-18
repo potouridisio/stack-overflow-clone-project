ba4d06228ed7b4dbef87b7fe5ebb2d373f49acad
Ioannis Potouridis <potouridisio@gmail.com>
2025-06-18 04:23:42 +0300

    Render tag names and occurrences

--- src/routes/Tags.jsx
+++ src/routes/Tags.jsx
@@ -19,10 +19,20 @@ export default function Tags() {
     <div className="grid grid-cols-4 gap-4">
       {tags.map((tag) => (
         <div
-          className="rounded border border-gray-300 p-4 text-sm"
+          className="flex flex-col items-start space-y-2 rounded border border-gray-300 p-4 text-sm text-gray-900"
           key={tag.id}
         >
-          {tag.description}
+          <a
+            className="inline-flex h-6 items-center rounded bg-gray-100 px-2 text-xs font-medium text-gray-700 hover:bg-gray-300 hover:text-gray-900"
+            href="#"
+          >
+            {tag.name}
+          </a>
+          <p className="line-clamp-4 grow">{tag.description}</p>
+          <span className="text-xs text-gray-700">
+            {tag.occurrenceCount} question
+            {tag.occurrenceCount === 1 ? "" : "s"}
+          </span>
         </div>
       ))}
     </div>
