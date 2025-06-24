ee661e9c0cbac981c1651fa2e745d374b56ba02d
Ioannis Potouridis <potouridisio@gmail.com>
2025-06-24 16:12:42 +0300

    Initial commit for project header

--- src/routes/Layout.jsx
+++ src/routes/Layout.jsx
@@ -2,30 +2,33 @@ import { NavLink, Outlet } from "react-router";
 
 export default function Layout() {
   return (
-    <div className="mx-auto flex max-w-7xl">
-      <aside className="w-40 flex-none border-r border-r-gray-300 py-6">
-        <nav>
-          <ul>
-            {[
-              ["Home", "/"],
-              ["Questions", "/questions"],
-              ["Tags", "/tags"],
-            ].map(([title, url]) => (
-              <li key={url}>
-                <NavLink
-                  className={({ isActive }) =>
-                    `flex items-center rounded-l-lg p-2 text-sm ${isActive ? "bg-gray-100 font-bold text-gray-900" : "text-gray-700 hover:bg-gray-50 hover:text-gray-900"}`
-                  }
-                  to={url}
-                >
-                  {title}
-                </NavLink>
-              </li>
-            ))}
-          </ul>
-        </nav>
-      </aside>
-      <Outlet />
-    </div>
+    <>
+      <header className="h-14 border-t-3 border-b border-t-orange-500 border-b-gray-300"></header>
+      <div className="mx-auto flex max-w-7xl">
+        <aside className="w-40 flex-none border-r border-r-gray-300 py-6">
+          <nav>
+            <ul>
+              {[
+                ["Home", "/"],
+                ["Questions", "/questions"],
+                ["Tags", "/tags"],
+              ].map(([title, url]) => (
+                <li key={url}>
+                  <NavLink
+                    className={({ isActive }) =>
+                      `flex items-center rounded-l-lg p-2 text-sm ${isActive ? "bg-gray-100 font-bold text-gray-900" : "text-gray-700 hover:bg-gray-50 hover:text-gray-900"}`
+                    }
+                    to={url}
+                  >
+                    {title}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </aside>
+        <Outlet />
+      </div>
+    </>
   );
 }
